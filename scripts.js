1f76d709540400168eb1d17f2cb0b42a338ec31a
Miroslavv <mbochko99@gmail.com>
2024-09-01 23:55:46 +0300

    Project end

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-let round = 5;
 let computerScore = 0;
 let humanScore = 0;
 
@@ -6,6 +5,7 @@ const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
 const scoreDiv = document.getElementById("score");
+const againButton = document.getElementById("again");
 
 function getComputerChoice() {
   let choice = Math.random();
@@ -22,17 +22,17 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
   let resultMessage;
   if (humanChoice === computerChoice) {
-    resultMessage = `Your choice - ${humanChoice}, computer choice - ${computerChoice} - DRAW!, [H: ${humanScore} | C: ${computerScore}]`;
+    resultMessage = `Your choice - ${humanChoice} <br>computer choice - ${computerChoice}  <br>DRAW!, [H: ${humanScore} | C: ${computerScore}]`;
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "paper") ||
     (humanChoice === "paper" && computerChoice === "rock")
   ) {
     humanScore++;
-    resultMessage = `Your choice - ${humanChoice} <br>computer choice - ${computerChoice} - YOU WIN!,  [H: ${humanScore} | C: ${computerScore}]`;
+    resultMessage = `Your choice - ${humanChoice} <br>computer choice - ${computerChoice}  <br>YOU WIN!,  [H: ${humanScore} | C: ${computerScore}]`;
   } else {
     computerScore++;
-    resultMessage = `Your choice - ${humanChoice} <br>computer choice - ${computerChoice} - COMPUTER WINS!,  [H: ${humanScore} | C: ${computerScore}]`;
+    resultMessage = `Your choice - ${humanChoice} <br>computer choice - ${computerChoice}  <br>COMPUTER WINS!,  [H: ${humanScore} | C: ${computerScore}]`;
   }
 
   scoreDiv.innerHTML = resultMessage;
@@ -40,22 +40,26 @@ function playRound(humanChoice, computerChoice) {
   if (computerScore === 5 || humanScore === 5) {
     if (computerScore > humanScore) {
       scoreDiv.innerHTML =
-        "You Lose!<br>" + `You: ${humanScore}, Computer: ${computerScore}`;
+        "YOU LOSE! <br>" +
+        `Your Score: ${humanScore} ||  Computer Score: ${computerScore}`;
       scoreDiv.style.backgroundColor = "#760f1d";
     } else {
       scoreDiv.innerHTML =
-        "You Win! <br>" +
+        "YOU WIN! <br>" +
         "" +
-        `You: ${humanScore}, Computer: ${computerScore} `;
+        `Yor Score: ${humanScore} || Computer Score: ${computerScore} `;
       scoreDiv.style.backgroundColor = "#0f766e";
     }
-    let playAgain = confirm("Do you want to play again?");
 
-    if (playAgain === true) {
+    if (computerScore === 5 || humanScore === 5) {
+      alert("Result is Ready, please click to play again button");
+    }
+    againButton.addEventListener("click", () => {
       humanScore = 0;
       computerScore = 0;
-      return playRound;
-    }
+      scoreDiv.style.backgroundColor = "";
+      playRound("nothing", "nothing");
+    });
   }
 }
 
@@ -68,38 +72,3 @@ paperButton.addEventListener("click", () =>
 scissorsButton.addEventListener("click", () =>
   playRound("scissors", getComputerChoice())
 );
-
-function getGreen() {
-  if (humanScore === 5) {
-  }
-}
-
-// function playGame() {
-//   for (let i = 0; i < round; i++) {
-//     let humanSelection = getHumanChoice();
-//     let computerSelection = getComputerChoice();
-//     playRound(humanSelection, computerSelection);
-//   }
-
-//   if (humanScore > computerScore) {
-//     alert(
-//       `You have ${humanScore}, computer has ${computerScore}, YOU WIN THIS GAME!`
-//     );
-//   } else if (humanScore === computerScore) {
-//     alert(`You have ${humanScore}, computer has ${computerScore}, IT's DRAW!`);
-//   } else {
-//     alert(
-//       `You have ${humanScore}, computer has ${computerScore}, COMPUTER WIN THIS GAME!`
-//     );
-//   }
-
-//   let playAgain = confirm("Do you want to play again?");
-
-//   if (playAgain === true) {
-//     humanScore = 0;
-//     computerScore = 0;
-//     return playGame();
-//   }
-// }
-
-// playGame();
